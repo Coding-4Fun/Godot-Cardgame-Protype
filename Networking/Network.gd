263d12e492a8518d3f39bbb6fee7b914a0ff93c7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2021-03-30 22:02:06 +0200

    Add: Globales singleton Script für Netzwerk funktionaltät

---
+++ Networking/Network.gd
@@ -0,0 +1,115 @@
+#class_name Template_Script
+
+#const Template = preload("res://Script_Templates/Template_Script.gd")
+
+#Nutze die Vergangenheitsform um Signale zu benennen:
+#
+#signal door_opened
+#signal score_changed
+
+#Verwende CONSTANT_CASE, alles in Großbuchstaben, mit einem Unterstrich zur Worttrennung:
+#const CONSTAND_VAR = 200:
+
+#Verwenden Sie PascalCase für Aufzählungsnamen und CONSTANT _CASE für deren Mitglieder, da es sich um Konstanten handelt:
+
+# enum Element {
+#    EARTH,
+#    WATER,
+#    AIR,
+#    FIRE,
+#}
+
+#-------------------------------------------------------------------------------
+
+#01. tool
+#02. class_name
+#03. extends
+#04. # docstring
+#
+#05. signals
+#06. enums
+#07. constants
+#08. exported variables
+#09. public variables
+#10. private variables
+#11. onready variables
+#
+#12. optional built-in virtual _init method
+#13. built-in virtual _ready method
+#14. remaining built-in virtual methods
+#15. public methods
+#16. private methods
+
+#-------------------------------------------------------------------------------
+# Header
+#-------------------------------------------------------------------------------
+#tool
+#class_name
+extends Node
+# Eine Vorlage für neue Scripte
+#-------------------------------------------------------------------------------
+# Signals
+signal player_disconnected
+signal server_disconnected
+#-------------------------------------------------------------------------------
+# enums
+#-------------------------------------------------------------------------------
+# constants
+const DEFAULT_IP = "127.0.0.1"
+const DEFAULT_PORT = 32023
+const MAX_PLAYERS = 2
+#-------------------------------------------------------------------------------
+# exported variables
+#-------------------------------------------------------------------------------
+# private variables
+var local_player_id : int = 0
+#-------------------------------------------------------------------------------
+# onready variables
+#onready var myVar: string
+#-------------------------------------------------------------------------------
+# optional built-in virtual _init method
+#-------------------------------------------------------------------------------
+# built-in virtual _ready method
+#-------------------------------------------------------------------------------
+# remaining built-in virtual methods
+func _ready() -> void:
+	get_tree().connect("network_peer_disconnected", self, "_on_player_disconnected")
+	get_tree().connect("network_peer_connected", self, "_on_player_connected")
+
+#-------------------------------------------------------------------------------
+# public methods
+func create_server() -> void:
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(DEFAULT_PORT, MAX_PLAYERS)
+	get_tree().set_network_peer(peer)
+	set_local_player_id()
+	print("Server wurde erstellt")
+	print("I am " + str(local_player_id))
+
+
+func set_local_player_id() -> void :
+	local_player_id = get_tree().get_network_unique_id()
+
+
+func connect_to_server() -> void:
+	var peer = NetworkedMultiplayerENet.new()
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	peer.create_client(DEFAULT_IP, DEFAULT_PORT)
+	get_tree().set_network_peer(peer)
+	set_local_player_id()
+#-------------------------------------------------------------------------------
+# private methods
+func _connected_to_server() -> void:
+	rpc("_send_player_info", local_player_id)
+
+
+func _on_player_connected(id:int) -> void:
+	if not get_tree().is_network_server ():
+		print(str(id) + " hat sich verbunden")
+	pass
+#-------------------------------------------------------------------------------
+# remote RPC methods
+remote func _send_player_info(id: int) -> void:
+	if get_tree().is_network_server ():
+		print(str(id) + " ID ist jetzt verbunden")
+
