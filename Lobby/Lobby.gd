0e1dbc2804d5c95b158f1a37dfb9276c2a0dc8bb
Fox-Alpha <fox-alpha@tarnhoerner.de>
2021-03-31 18:12:29 +0200

    Chg: Variablen für Textboxen zugriff erstellt Add: Signals zum speichern Chg: Speichern der eingabedaten per Signal

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,7 +1,19 @@
 extends Control
 
+onready var UI_NameBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/NameTextbox
+onready var UI_HostIPBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/IPTextBox
+onready var UI_HostPortBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/PortTextbox
+onready var UI_MayPlayerBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/MaxPlayersTextbox
+
+
+signal playername_changed (string)
+signal hostip_changed(string)
+signal hostport_changed(string)
+signal maxplayers_changed(string)
+
+
 func _ready() -> void:
-	$VBoxContainer/CenterContainer/GridContainer/NameTextbox.text = Saved.config_data["Player_name"]
+	UI_NameBox.text = Config.config_data["Player_name"]
 
 
 func _on_HostButton_pressed() -> void:
@@ -13,13 +25,17 @@ func _on_JoinButton_pressed() -> void:
 
 
 func _on_NameTextbox_text_entered(new_text: String) -> void:
-	pass # Replace with function body.
+	emit_signal("playername_changed", new_text)
 
 
 func _on_IPTextbox_text_entered(new_text: String) -> void:
-	pass # Replace with function body.
+	emit_signal("hostip_changed", new_text)
 
 
 func _on_PortTextbox_text_entered(new_text: String) -> void:
-	pass # Replace with function body.
+	emit_signal("hostport_changed", new_text)
 
+
+
+func _on_MaxPlayersTextbox_text_entered(new_int: int) -> void:
+	emit_signal("maxplayers_changed", new_int)
