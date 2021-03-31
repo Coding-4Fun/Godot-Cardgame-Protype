f59997bb9839c56157c3f38823757151c0a20f34
Fox-Alpha <fox-alpha@tarnhoerner.de>
2021-03-31 19:17:14 +0200

    Add: Verweise auf andere TextBoxen Chg: Textbox->Changed weitergabe als integer

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,19 +1,25 @@
 extends Control
 
 onready var UI_NameBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/NameTextbox
-onready var UI_HostIPBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/IPTextBox
+onready var UI_HostIPBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/IPTextbox
 onready var UI_HostPortBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/PortTextbox
 onready var UI_MayPlayerBox:LineEdit = $VBoxContainer/CenterContainer/GridContainer/MaxPlayersTextbox
 
-
-signal playername_changed (string)
-signal hostip_changed(string)
-signal hostport_changed(string)
-signal maxplayers_changed(string)
+signal playername_changed
+#(newname)
+signal hostip_changed
+#(newip)
+signal hostport_changed
+#(newport)
+signal maxplayers_changed
+#(newplayer)
 
 
 func _ready() -> void:
 	UI_NameBox.text = Config.config_data["Player_name"]
+	UI_HostIPBox.text = Config.config_data["Network"]["HostIPAdress"]
+	UI_HostPortBox.text = str(Config.config_data["Network"]["HostGamePort"])
+	UI_MayPlayerBox.text = str(Config.config_data["Network"]["MaxPlayers"])
 
 
 func _on_HostButton_pressed() -> void:
@@ -33,9 +39,8 @@ func _on_IPTextbox_text_entered(new_text: String) -> void:
 
 
 func _on_PortTextbox_text_entered(new_text: String) -> void:
-	emit_signal("hostport_changed", new_text)
+	emit_signal("hostport_changed", int(new_text))
 
 
-
-func _on_MaxPlayersTextbox_text_entered(new_int: int) -> void:
-	emit_signal("maxplayers_changed", new_int)
+func _on_MaxPlayersTextbox_text_entered(new_text: String) -> void:
+	emit_signal("maxplayers_changed", int(new_text))
