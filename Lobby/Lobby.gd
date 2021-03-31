33435d6c03cec6b3db8b8175909c47a3f1f8fda4
Fox-Alpha <fox-alpha@tarnhoerner.de>
2021-03-31 15:54:53 +0200

    Add: Label und TextBox für Maximale Anzahl Clients hinzugefügt Add: Signals für Eingabe Events hinzugefügt

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready() -> void:
-	$VBoxContainer/CenterContainer/GridContainer/NameTextbox.text = Saved.save_data["Player_name"]
+	$VBoxContainer/CenterContainer/GridContainer/NameTextbox.text = Saved.config_data["Player_name"]
 
 
 func _on_HostButton_pressed() -> void:
@@ -10,3 +10,16 @@ func _on_HostButton_pressed() -> void:
 
 func _on_JoinButton_pressed() -> void:
 	Network.connect_to_server()
+
+
+func _on_NameTextbox_text_entered(new_text: String) -> void:
+	pass # Replace with function body.
+
+
+func _on_IPTextbox_text_entered(new_text: String) -> void:
+	pass # Replace with function body.
+
+
+func _on_PortTextbox_text_entered(new_text: String) -> void:
+	pass # Replace with function body.
+
